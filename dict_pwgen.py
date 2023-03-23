294500a2d7af27ea18ada72543ac272be110894a
Brennan Hill <bhill298@gmail.com>
2023-03-22 19:30:57 -0600

    Clean up arg parsing

--- dict_pwgen.py
+++ dict_pwgen.py
@@ -52,20 +52,41 @@ def relpath(fname):
     return os.path.join(os.path.dirname(__file__), fname)
 
 
+def prob_arg(prob):
+    try:
+        prob = float(prob)
+        if prob < 0.0 or prob > 1.0:
+            raise ValueError()
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Probability should be a valid number from [0, 1] got {prob}")
+    return prob
+
+
+def positive_int_arg(i, disallow_zero=False):
+    try:
+        i = int(i)
+        if i < 0 or (disallow_zero and i == 0):
+            raise ValueError()
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Should be a valid int >{'' if disallow_zero else '='} 0, got {i}")
+    return i
+positive_int_arg_nonzero = lambda i: positive_int_arg(i, disallow_zero=True)
+
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     description="Generate passwords using dictionary words that are easier to remember")
 parser.add_argument('-i', "--input-dict", type=argparse.FileType('r'), action="append", help="use custom wordlist (can be passed multiple times)")
 parser.add_argument('-s', "--sciterms", action="store_true", help="use science terms dictionary (ignored with -i)")
 parser.add_argument('-j', "--jargon", action="store_true", help="use jargon (names / proper nouns, more complex words) dictionary (ignored with -i)")
-parser.add_argument('-m', "--min-wordlen", type=int, default=6, help="min length of words to use")
-parser.add_argument('-a', "--max-wordlen", type=int, default=float("inf"), help="max length of words to use")
-parser.add_argument('-n', "--num-words", type=int, default=4, help="number of words to generate")
+parser.add_argument('-m', "--min-wordlen", type=positive_int_arg, default=6, help="min length of words to use")
+parser.add_argument('-a', "--max-wordlen", type=positive_int_arg_nonzero, default=float("inf"), help="max length of words to use")
+parser.add_argument('-n', "--num-words", type=positive_int_arg_nonzero, default=4, help="number of words to generate")
 parser.add_argument('-y', "--allow-hyphen", action="store_true", help="allow words with hyphens")
-parser.add_argument('-t', "--trans-modify-prob", type=float, default=0.0, help="transform characters in words with some probability [0, 1] (e.g. 'a' -> @)")
-parser.add_argument('-u', "--upper-modify-prob", type=float, default=0.0, help="uppercase characters in words with some probability [0, 1]")
+parser.add_argument('-t', "--trans-modify-prob", type=prob_arg, default=0.0, help="transform characters in words with some probability [0, 1] (e.g. 'a' -> @)")
+parser.add_argument('-u', "--upper-modify-prob", type=prob_arg, default=0.0, help="uppercase characters in words with some probability [0, 1]")
 parser.add_argument('-U', "--always-upper-start", action="store_true",
     help="always make first character of each word uppercase (skips all other modifications for that character)")
-parser.add_argument('-c', "--add-char-prob", type=float, default=1.0, help="add number + symbols with some probability [0, 1]")
+parser.add_argument('-c', "--add-char-prob", type=prob_arg, default=1.0, help="add number + symbols with some probability [0, 1]")
 parser.add_argument('-w', "--add-char-where", choices=("between", "beforeafter", "everywhere"), default="between",
     help="where to add numbers + symbols (between words, before and after words, everywhere - including between characters)")
 parser.add_argument('-r', "--crack-times", action="store_true", help="Print estimate crack times for generated password (using zxcvbn)")
@@ -76,18 +97,8 @@ if args.check_crack_times is not None:
     print(get_password_crack_times(args.check_crack_times))
     sys.exit(0)
 
-if args.max_wordlen < 0:
-    raise ValueError(f"max wordlen should not be < 0, got {args.max_wordlen}")
-if args.min_wordlen < 0:
-    raise ValueError(f"min wordlen should not be < 0, got {args.min_wordlen}")
 if args.max_wordlen < args.min_wordlen:
     raise ValueError(f"max wordlen should not be < min wordlen ({args.max_wordlen} < {args.min_wordlen})")
-if args.trans_modify_prob < 0.0 or args.trans_modify_prob > 1.0:
-    raise ValueError(f"trans prob should be in [0, 1], got {args.trans_modify_prob}")
-if args.upper_modify_prob < 0.0 or args.upper_modify_prob > 1.0:
-    raise ValueError(f"upper prob should be in [0, 1], got {args.upper_modify_prob}")
-if args.add_char_prob < 0.0 or args.add_char_prob > 1.0:
-    raise ValueError(f"add char prob should be in [0, 1], got {args.add_char_prob}")
 
 symbols = "!@#$%^&_-?.,~\"'()*+/:;<>=`|"
 symbols_digits = string.digits + symbols
@@ -114,6 +125,7 @@ else:
         input_filenames.append("jargon.txt")
     for fname in input_filenames:
         input_files.append(open(relpath(fname)))
+
 words = set()
 for f in input_files:
     words = words.union(read_words(f))
@@ -121,6 +133,7 @@ for f in input_files:
 words = list({word for word in words if word_filter(word, args)})
 if len(words) < args.num_words:
     raise ValueError(f"Filtered wordlist has {len(words)} unique elements, which is too short for password length {args.num_words}")
+
 selection = random.sample(words, k=args.num_words)
 final_words = []
 for idx, word in enumerate(selection):
@@ -145,6 +158,7 @@ for idx, word in enumerate(selection):
         word = ''.join(word_list)
     final_words.append(word)
 pw = ''.join(final_words)
+
 print(pw)
 if args.crack_times:
     print()
